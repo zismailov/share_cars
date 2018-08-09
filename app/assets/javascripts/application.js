860f8e4cbc1375c887761c13f7fbeb421f6789c8
Zulkar <bedelbaev@gmail.com>
2018-08-09 12:52:41 +0600

    use jquery-turbolinks because of document bindings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,48 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
 //= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require jquery-ui/autocomplete
 //= require bootstrap-sprockets
-//= require activestorage
-//= require turbolinks
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
-//= require jquery-ui/autocomplete
-//= require_self
-
-
-$(function () {
-
-  $('.datepicker').datepicker({
-    language: 'en'
-  })
-
-  $("#search_from, #search_to").autocomplete({
-    source: function (request, response) {
-      $.getJSON("/geocodes/autocomplete?term=" + request.term, function (data) {
-        response($.map(data, function (el) {
-          return {
-            label: el.display_name,
-            value: el.display_name,
-            id: [el.lat, el.lon]
-          }
-        }))
-      })
-    },
-    minLength: 2,
-    select: function (event, ui) {
-      $('#' + this.id + '_coordinates').val(ui.item.id)
-    }
-  })
-
-})
+//= require _layout
+//= require _jumbotron
+//= require turbolinks
