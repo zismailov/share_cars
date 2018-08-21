fb9fb4707c882c1fd34d69df1f09a5e188c607b1
Zulkar <bedelbaev@gmail.com>
2018-08-21 15:03:03 +0600

    work on show trip (i18n, style and encode_decode)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def number_to_age(age)
     "#{age} years".html_safe
   end
+
+  def encode_decode(string)
+    string&.encode("iso-8859-1")&.force_encoding("utf-8") unless string.nil?
+  end
+  alias ed encode_decode
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,11 @@
+# config/initializers/locale.rb
+
 I18n.enforce_available_locales = false
-I18n.config.available_locales = :en
-Rails.application.config.i18n.default_locale = :en
+
+I18n.config.available_locales = [:en]
+
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+
+# Set default locale to something other than :en
+I18n.default_locale = :en
