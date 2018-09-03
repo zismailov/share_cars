fd84d63a93c381015c3864ea8c1371f9b581bf58
Zulkar <bedelbaev@gmail.com>
2018-09-03 14:30:20 +0600

    try with 10000m for scalingo postgis

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -71,7 +71,7 @@ class Trip < ApplicationRecord
             ST_Dwithin(
               ST_GeographyFromText('SRID=4326;POINT(' || points.lon || ' ' || points.lat || ')'),
               ST_GeographyFromText('SRID=4326;POINT(%f %f)'),
-              2000
+              10000
             )
           ) as trip_point_a
           inner join points on points.trip_id = trip_point_a.id
@@ -79,7 +79,7 @@ class Trip < ApplicationRecord
             ST_Dwithin(
               ST_GeographyFromText('SRID=4326;POINT(' || points.lon || ' ' || points.lat || ')'),
               ST_GeographyFromText('SRID=4326;POINT(%f %f)'),
-              2000
+              10000
             )
           and point_a_rank < points.rank
           order by departure_date asc
