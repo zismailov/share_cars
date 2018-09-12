88ecb5b948362431cec7db00b0cc19a3888c8791
Zulkar <bedelbaev@gmail.com>
2018-09-12 13:51:09 +0600

    reuse of nominatim, this time with cities only and showing postcodes

--- app/controllers/geocodes_controller.rb
+++ app/controllers/geocodes_controller.rb
@@ -1,21 +1,13 @@
 class GeocodesController < ApplicationController
-  # for Nominatim
-  #  COUNTRY_SEARCH_LIST = "kg,ru,kz".freeze
-  #  def autocomplete
-  #   render json: Geocoder.search(params[:term], params: {countrycodes: COUNTRY_SEARCH_LIST}).map(&:data)
-  #  end
+  COUNTRY_SEARCH_LIST = "kg,ru,kz".freeze
+
+  def autocomplete
+    @results = Geocoder.search(params[:term], countrycodes: COUNTRY_SEARCH_LIST).select { |res| res.data["address"]["city"] && res.data["address"]["postcode"] }
+  end
 
   # for Google
   #  Geocoder.search("Lille").map(&:data).first["geometry"]["location"]
   #  => {"lat"=>50.62925, "lng"=>3.057256}
   #  Geocoder.search("Lille").map(&:data).first["address_components"][0]["short_name"]
   #  => "Lille"
-
-  #
-  # @returns :
-  #  [{ display_name: "Lille", lat: ..., lon: ...}, {}]
-  #
-  def autocomplete
-    @results = Geocoder.search(params[:term])
-  end
 end
