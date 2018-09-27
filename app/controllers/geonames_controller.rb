e3efa074385ad90563185a295359f0379560849e
Zulkar <bedelbaev@gmail.com>
2018-09-27 16:38:31 +0600

    limit searchick to 10

--- app/controllers/geonames_controller.rb
+++ app/controllers/geonames_controller.rb
@@ -2,6 +2,6 @@ class GeonamesController < ApplicationController
   COUNTRY_SEARCH_LIST = "kg,ru,kz".freeze
 
   def autocomplete
-    @results = Geoname.search(params[:term])
+    @results = Geoname.search(params[:term], limit: 10)
   end
 end
