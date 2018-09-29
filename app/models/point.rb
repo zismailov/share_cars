21ef707fb04260ad95179ca094673c2406baf318
Zulkar <bedelbaev@gmail.com>
2018-09-29 16:46:06 +0600

    add missing gps coordinates explanation

--- app/models/point.rb
+++ app/models/point.rb
@@ -6,10 +6,15 @@ class Point < ApplicationRecord
   validates_presence_of :kind, :rank, :trip, :lat, :lon, :city
   validates_inclusion_of :kind, in: KINDS
   validates_numericality_of :rank
+  validate :lat_lon_must_be_set
 
   before_validation :set_from_rank, :set_to_rank
 
   private
+
+  def lat_lon_must_be_set
+    errors.add(:city, "You must select a city from the list") if lat.blank? || lon.blank?
+  end
 
   def set_from_rank
     self.rank = 0 if kind == "From"
